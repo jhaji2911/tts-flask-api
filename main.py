7325e7416ca94cb101e619061c5b33835ac41073
Nishant <jhanishant2000@gmail.com>
2024-07-09 19:51:15 +0530

    new changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from distutils.log import debug
 from app import app
 
 def main():
-    app.run(debug=1, host="::", port="5000")
+    app.run(debug=1, host="::", port="5001")
 
 if __name__ == "__main__":
     main()
